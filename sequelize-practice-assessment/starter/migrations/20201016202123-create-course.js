3bea884254e39de57f5b7444e9774f858cd2f9e3
Daniel TIllero <danieltillero67@gmail.com>
2020-10-16 16:21:39 -0400

    create course model/migration

---
+++ sequelize-practice-assessment/starter/migrations/20201016202123-create-course.js
@@ -0,0 +1,36 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('Courses', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        type: Sequelize.STRING
+      },
+      level: {
+        type: Sequelize.INTEGER
+      },
+      campusId: {
+        type: Sequelize.INTEGER
+      },
+      departmentId: {
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('Courses');
+  }
+};

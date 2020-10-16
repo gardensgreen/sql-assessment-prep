c841b0b155abf5ebe0a9844d2e68d99f1e94547a
Daniel TIllero <danieltillero67@gmail.com>
2020-10-16 16:17:31 -0400

    Update migration

--- sequelize-practice-assessment/starter/migrations/20201016201603-create-person.js
+++ sequelize-practice-assessment/starter/migrations/20201016201603-create-person.js
@@ -1,33 +1,36 @@
-'use strict';
+"use strict";
 module.exports = {
-  up: (queryInterface, Sequelize) => {
-    return queryInterface.createTable('People', {
-      id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.INTEGER
-      },
-      firstName: {
-        type: Sequelize.STRING
-      },
-      lastName: {
-        type: Sequelize.STRING
-      },
-      email: {
-        type: Sequelize.STRING
-      },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      }
-    });
-  },
-  down: (queryInterface, Sequelize) => {
-    return queryInterface.dropTable('People');
-  }
+    up: (queryInterface, Sequelize) => {
+        return queryInterface.createTable("People", {
+            id: {
+                allowNull: false,
+                autoIncrement: true,
+                primaryKey: true,
+                type: Sequelize.INTEGER,
+            },
+            firstName: {
+                allowNull: false,
+                type: Sequelize.STRING(50),
+            },
+            lastName: {
+                allowNull: false,
+                type: Sequelize.STRING(50),
+            },
+            email: {
+                allowNull: false,
+                type: Sequelize.STRING(255),
+            },
+            createdAt: {
+                allowNull: false,
+                type: Sequelize.DATE,
+            },
+            updatedAt: {
+                allowNull: false,
+                type: Sequelize.DATE,
+            },
+        });
+    },
+    down: (queryInterface, Sequelize) => {
+        return queryInterface.dropTable("People");
+    },
 };

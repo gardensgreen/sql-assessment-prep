d11f362860d37c5666c96f77870db1bc9a5e6d99
Daniel TIllero <danieltillero67@gmail.com>
2020-10-16 16:34:02 -0400

    Generate seed file for courses and quickfix departments seed file

---
+++ sequelize-practice-assessment/starter/seeders/20201016203314-Courses.js
@@ -0,0 +1,78 @@
+"use strict";
+
+module.exports = {
+    up: (queryInterface, Sequelize) => {
+        return queryInterface.bulkInsert(
+            "Courses",
+            [
+                {
+                    name: "Macro",
+                    level: 860,
+                    campusId: 2,
+                    departmentId: 1,
+                    createdAt: new Date(),
+                    updatedAt: new Date(),
+                },
+                {
+                    name: "Calculus",
+                    level: 830,
+                    campusId: 2,
+                    departmentId: 2,
+                    createdAt: new Date(),
+                    updatedAt: new Date(),
+                },
+                {
+                    name: "Discrete",
+                    level: 690,
+                    campusId: 4,
+                    departmentId: 2,
+                    createdAt: new Date(),
+                    updatedAt: new Date(),
+                },
+                {
+                    name: "Orchestra",
+                    level: 280,
+                    campusId: 1,
+                    departmentId: 3,
+                    createdAt: new Date(),
+                    updatedAt: new Date(),
+                },
+                {
+                    name: "Appreciation",
+                    level: 650,
+                    campusId: 1,
+                    departmentId: 3,
+                    createdAt: new Date(),
+                    updatedAt: new Date(),
+                },
+                {
+                    name: "Supply Chain",
+                    level: 790,
+                    campusId: 2,
+                    departmentId: 1,
+                    createdAt: new Date(),
+                    updatedAt: new Date(),
+                },
+                {
+                    name: "Algebra",
+                    level: 300,
+                    campusId: 2,
+                    departmentId: 2,
+                    createdAt: new Date(),
+                    updatedAt: new Date(),
+                },
+            ],
+            {}
+        );
+    },
+
+    down: (queryInterface, Sequelize) => {
+        /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.bulkDelete('People', null, {});
+    */
+    },
+};

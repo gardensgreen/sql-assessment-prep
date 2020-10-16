c3cd90822c71f5d84d60f711b8a7ffc9e24ffe64
Daniel TIllero <danieltillero67@gmail.com>
2020-10-16 16:32:02 -0400

    update migration for enrollment, create seed file for People and Campuses

--- sequelize-practice-assessment/starter/migrations/20201016202700-create-enrollment.js
+++ sequelize-practice-assessment/starter/migrations/20201016202700-create-enrollment.js
@@ -1,30 +1,32 @@
-'use strict';
+"use strict";
 module.exports = {
-  up: (queryInterface, Sequelize) => {
-    return queryInterface.createTable('Enrollments', {
-      id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.INTEGER
-      },
-      personId: {
-        type: Sequelize.INTEGER
-      },
-      courseId: {
-        type: Sequelize.INTEGER
-      },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      }
-    });
-  },
-  down: (queryInterface, Sequelize) => {
-    return queryInterface.dropTable('Enrollments');
-  }
+    up: (queryInterface, Sequelize) => {
+        return queryInterface.createTable("Enrollments", {
+            id: {
+                allowNull: false,
+                autoIncrement: true,
+                primaryKey: true,
+                type: Sequelize.INTEGER,
+            },
+            personId: {
+                type: Sequelize.INTEGER,
+                references: { model: "People" },
+            },
+            courseId: {
+                type: Sequelize.INTEGER,
+                references: { model: "Courses" },
+            },
+            createdAt: {
+                allowNull: false,
+                type: Sequelize.DATE,
+            },
+            updatedAt: {
+                allowNull: false,
+                type: Sequelize.DATE,
+            },
+        });
+    },
+    down: (queryInterface, Sequelize) => {
+        return queryInterface.dropTable("Enrollments");
+    },
 };
---
+++ sequelize-practice-assessment/starter/seeders/20201016203040-Campuses.js
@@ -0,0 +1,42 @@
+"use strict";
+
+module.exports = {
+    up: (queryInterface, Sequelize) => {
+        return queryInterface.bulkInsert(
+            "Campuses",
+            [
+                {
+                    name: "Valdivia",
+                    createdAt: new Date(),
+                    updatedAt: new Date(),
+                },
+                {
+                    name: "Bangor",
+                    createdAt: new Date(),
+                    updatedAt: new Date(),
+                },
+                {
+                    name: "Chatillon",
+                    createdAt: new Date(),
+                    updatedAt: new Date(),
+                },
+                {
+                    name: "Filacciano",
+                    createdAt: new Date(),
+                    updatedAt: new Date(),
+                },
+            ],
+            {}
+        );
+    },
+
+    down: (queryInterface, Sequelize) => {
+        /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.bulkDelete('People', null, {});
+    */
+    },
+};

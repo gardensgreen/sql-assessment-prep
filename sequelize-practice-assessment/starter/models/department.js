fc0bdc98f5c2fa84d357aedb84f23ed05690e1bf
Daniel TIllero <danieltillero67@gmail.com>
2020-10-16 16:20:30 -0400

    update department migration

--- sequelize-practice-assessment/starter/models/department.js
+++ sequelize-practice-assessment/starter/models/department.js
@@ -1,10 +1,16 @@
-'use strict';
+"use strict";
 module.exports = (sequelize, DataTypes) => {
-  const Department = sequelize.define('Department', {
-    name: DataTypes.STRING
-  }, {});
-  Department.associate = function(models) {
-    // associations can be defined here
-  };
-  return Department;
+    const Department = sequelize.define(
+        "Department",
+        {
+            name: DataTypes.STRING,
+            allowNull: false,
+            unique: true,
+        },
+        {}
+    );
+    Department.associate = function (models) {
+        // associations can be defined here
+    };
+    return Department;
 };

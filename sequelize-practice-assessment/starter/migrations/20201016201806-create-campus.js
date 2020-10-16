1c3043ff879fa9f41c4f43289ee9e706bcaa232f
Daniel TIllero <danieltillero67@gmail.com>
2020-10-16 16:19:09 -0400

    Update create campus migration

--- sequelize-practice-assessment/starter/migrations/20201016201806-create-campus.js
+++ sequelize-practice-assessment/starter/migrations/20201016201806-create-campus.js
@@ -1,27 +1,29 @@
-'use strict';
+"use strict";
 module.exports = {
-  up: (queryInterface, Sequelize) => {
-    return queryInterface.createTable('Campuses', {
-      id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.INTEGER
-      },
-      name: {
-        type: Sequelize.STRING
-      },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      }
-    });
-  },
-  down: (queryInterface, Sequelize) => {
-    return queryInterface.dropTable('Campuses');
-  }
+    up: (queryInterface, Sequelize) => {
+        return queryInterface.createTable("Campuses", {
+            id: {
+                allowNull: false,
+                autoIncrement: true,
+                primaryKey: true,
+                type: Sequelize.INTEGER,
+            },
+            name: {
+                allowNull: false,
+                type: Sequelize.STRING(255),
+                unique: true,
+            },
+            createdAt: {
+                allowNull: false,
+                type: Sequelize.DATE,
+            },
+            updatedAt: {
+                allowNull: false,
+                type: Sequelize.DATE,
+            },
+        });
+    },
+    down: (queryInterface, Sequelize) => {
+        return queryInterface.dropTable("Campuses");
+    },
 };
